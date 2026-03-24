ed1d9d9120734aac17819ed8eebe28f29a92612e
choi minju <chlminju32@gmail.com>
2026-03-24 16:33:50 +0900

    메인1

--- STL/메인.cpp
+++ STL/메인.cpp
@@ -9,65 +9,44 @@
 //               - C/C++ 언어 - SDL 검사 - 아니오
 
 // [메모]
-// 많은 수의 자료 다루기 - FILE I/O - binary I/O - class 객체
+// 동적 메모리 할당(dynamic memory allocation)
 
 #include <iostream>
-#include <algorithm>
-#include <random>
-#include <print>
-#include <string>
-#include <array>
-#include <fstream>
-#include <filesystem>
 #include "save.h"
 using namespace std;
 
-default_random_engine dre;
-uniform_int_distribution uid{ 0, 9999 };
-uniform_int_distribution uidNameLen{ 10, 30 };
-uniform_int_distribution<int> uidChar{ 'a', 'z' };
-
-class Dog {
-private:
-	string name;  // [10, 30] 사이의 소문자로 구성된 이름
-	size_t id;    // [0, 9999]
-
-	friend ostream& operator <<(ostream& os, const Dog& dog) {
-		print(os, "[{:>4}] - {} ", dog.id, dog.name);
-		return os;
-	}
-
-	friend istream& operator >> (istream& is, Dog& dog) {
-		is.read((char*)&dog, sizeof(Dog));
-		return is;
-	}
-};
-
-// [문제] 파일 "Dog천마리"에는 class Dog 객체 1000마리가 저장되어있다.
-// 파일은 binary모드이고 각 객체는 메모리 크기 그대로 stream의 write함수로 기록하였다
-// 모든 객체를 한번의 write함수로 기록하였다.
-// Dog의 맴버는 위의 코드와 같다
-// 메모리에 모두 읽어오시오
-// 메모리에 읽은 모든 Dog를 화면 출력하라.
-
+// [문제] 사용자가 입력한 수만큼 int를 저장할 메모리를 확보하라
+// 1부터 시작하는 정수로 메모리를 채워라
+// 합계를 화면에 출력하라
 
 int main()
 {
-	Dog dog;
-	array<Dog, 1000> dogs;
+	cout << "입력 : ";
+	size_t num;
+	cin >> num;
 
-	ifstream in{ "Dog천마리", ios::binary };
-	if (not in) {
-		cout << "dog no" << endl;
-		return 111;
+	int* p = new int[num];
+	
+	/*
+	while(true) {
+		new int[num];
+	}
+	이런건 안됨 -> 
+	*/
+
+	for (int i = 0; i < num; ++i) {
+		p[i] = i + 1;
 	}
 
-	array<Dog, 1'000> dogs;
-	in.read((char*)dogs.data(), dogs.size() * sizeof(Dog));
-
-	for (const Dog dog : dogs) { // 꼭 const를 붙여햐됨
-		cout << dog << endl;
+	int sum{};
+	for (int i = 0; i < num; ++i) {
+		sum += p[i];
 	}
+
+	cout << "1부터" << num << " 까지의 합계 : " << sum << endl;
+	delete[] p;
+
+
 
 	save("메인.cpp");
 }
