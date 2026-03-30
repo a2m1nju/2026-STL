6c9c07db1af039045dfb2a2f945b5a1497e65351
choi minju <chlminju32@gmail.com>
2026-03-30 13:55:30 +0900

    Dog 생성, 소멸

--- STL/메인.cpp
+++ STL/메인.cpp
@@ -11,28 +11,27 @@
 // [메모]
 // 스마트 포인터 -> 콜러블 타입 -> 실습 -> STL
 
-#include <iostream>
-#include "save.h"
-using namespace std;
-
 // c++언어의 보증사항
 // 프로그램이 정상 종료시 (return문, exit와는 다름) 스택에 생성된 지역객체를
 // 반드시 소멸됨을 보장한다.
 
+#include <iostream>
+#include "save.h"
+using namespace std;
+
+class Dog {
+public:
+	Dog() { cout << "생성" << endl; }
+	~Dog() { cout << "소멸" << endl; }
+};
+
 int main()
 {
-	int* p = new int{ 20260330 };
-
-	cout << *p << endl;
-
-	return 0; // 메모리 leakage, 고갈
-
-	cout << "이 문장은 출력 안됨" << endl;
-
 	save("메인.cpp");
+	
+	Dog* p = new Dog;
+
 
 	delete p;
 }
 
-// 리턴문으로 끝낼때 뭐 선언된 지역객체? p가 사라짐을 보장한다?
-// 예외가 던져졌을때도 예외를 잘ㄹ 처리하면 지역에서 만든 객체는 반드시 지워짐을 보장함
