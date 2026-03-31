2edfbbfec5d935c639f7ef91243212ec75fc5d30
choi minju <chlminju32@gmail.com>
2026-03-31 16:59:48 +0900

    점프, 슬라이드

--- STL/메인.cpp
+++ STL/메인.cpp
@@ -11,23 +11,31 @@
 // [메모]
 // CALLABLE TYPES 콜러블 타입
 // 1. 함수
+// -> 
 // 2. lambda
 
 #include <iostream>
 #include "save.h"
 using namespace std;
 
-void f() {
-	cout << "호출 가능" << endl;
+void 점프() {
+	cout << "점프" << endl;
+}
+
+void 슬라이드() {
+	cout << "슬라이드" << endl;
 }
 
 int main()
 {
-	void (*a)() = f; //  a는 함수 f를 가리키는 포인터
 
-	a();
+	void(*왼쪽키)() = 점프;
+	왼쪽키();
+	왼쪽키();
+	// 설정에서 키 기능 변경
+	왼쪽키 = 슬라이드;
 
-
+	왼쪽키();
 
 	save("메인.cpp");
 	
