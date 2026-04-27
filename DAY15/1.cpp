d3fd0b429ad34fbf1e39d92ed40083d8b1c6cede
choi minju <chlminju32@gmail.com>
2026-04-27 13:59:32 +0900

    v.reserve(10)

--- DAY15/1.cpp
+++ DAY15/1.cpp
@@ -30,14 +30,7 @@ int main()
 {
 	save("메인.cpp");
 
-	vector<ZString> v{ "1","22","333" };
-	v.reserve(10); // 공간 늘리기
-
-	cout << "v의 원소에 접근" << endl;
-	v.push_back("4444");
-	v.push_back("55555");  // -> 이렇게 사용하면 안됨 -> 매번 메모리를 움직여야하기 때문에
-	v.push_back("666666"); // -> 해결방법 : 공간을 늘린다
-
+	vector<ZString> v{ "!","22","333","4444","55555" };
 	cout << "v의 원소에 접근" << endl;
 	for (ZString& zs : v) { // 레퍼런스를 붙여서 원본을 사용하도록 함
 		cout << zs << endl;
